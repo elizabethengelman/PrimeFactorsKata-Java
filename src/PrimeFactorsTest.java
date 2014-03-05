9c8a07f6b4f193fe110ea9bf966c900c77758fed
Elizabeth Engelman <emengelman@gmail.com>
2014-03-05 15:41:37 -0600

    refactored var expectedFactors into a method in the test class

--- src/PrimeFactorsTest.java
+++ src/PrimeFactorsTest.java
@@ -9,21 +9,27 @@ import static junit.framework.Assert.assertEquals;
  * Created by elizabethengelman on 3/5/14.
  */
 public class PrimeFactorsTest {
-    List<Integer> expectedFactors = new ArrayList<Integer>();
+
+    public List<Integer> expectedFactors(int... numbers){
+        List<Integer> factors = new ArrayList<Integer>();
+        for (int n : numbers){
+            factors.add(n);
+        }
+        return factors;
+    }
+
     @Test
     public void factorsForOne(){
-        assertEquals(expectedFactors, PrimeFactors.generate(1));
+        assertEquals(expectedFactors(), PrimeFactors.generate(1));
     }
 
     @Test
     public void factorsForTwo(){
-        expectedFactors.add(2);
-        assertEquals(expectedFactors, PrimeFactors.generate(2));
+        assertEquals(expectedFactors(2), PrimeFactors.generate(2));
     }
 
     @Test
     public void factorsForThree(){
-        expectedFactors.add(3);
-        assertEquals(expectedFactors, PrimeFactors.generate(3));
+        assertEquals(expectedFactors(3), PrimeFactors.generate(3));
     }
 }
